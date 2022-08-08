68c35e0bfc978fc718f2f432d5cd6b2aabc5a77e
chrissiku <chrissiku5@gmail.com>
2022-08-08 19:09:58 +0200

    List the name of all animals born between 2016 and 2019.

--- queries.sql
+++ queries.sql
@@ -3,3 +3,8 @@
 select *
 from animals
 WHERE name LIKE '%mon';
+
+-- List the name of all animals born between 2016 and 2019.
+select * from animals
+WHERE date_of_birth 
+BETWEEN '01/01/2016' AND '31/12/2019';
