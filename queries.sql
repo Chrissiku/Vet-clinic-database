b445797e217bae7dea87e6811bee769be9310e1e
chrissiku <chrissiku5@gmail.com>
2022-08-08 19:21:45 +0200

    List name and escape attempts of animals that weigh more than 10.5kg

--- queries.sql
+++ queries.sql
@@ -1,21 +1,29 @@
 /*Queries that provide answers to the questions from all projects.*/
 -- Find all animals whose name ends in "mon".
 SELECT *
-from animals
+FROM animals
 WHERE name LIKE '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
-SELECT * from animals
+SELECT * 
+FROM animals
 WHERE date_of_birth 
 BETWEEN '01/01/2016' AND '31/12/2019';
 
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
-SELECT name from animals
+SELECT name 
+FROM animals
 WHERE neutered=TRUE 
 AND escape_attempts < 3;
 
 -- List the date of birth of all animals named either "Agumon" or "Pikachu".
-SELECT date_of_birth from animals
+SELECT date_of_birth 
+FROM animals
 WHERE name='Agumon' 
 OR name='Pikachu';
+
+-- List name and escape attempts of animals that weigh more than 10.5kg
+SELECT name,escape_attempts 
+FROM animals
+WHERE weight_kg > 10.5;
 
