a6e98d2079165c5537b000e779fb23d647947c8e
chrissiku <chrissiku5@gmail.com>
2022-08-08 18:59:21 +0200

    Insert into animals table

--- data.sql
+++ data.sql
@@ -1,1 +1,16 @@
 /* Populate database with sample data. */
+-- Instert data into animals table.
+INSERT INTO
+    animals(
+        id,
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+(1, 'Agumon', '03/02/2020', 0, TRUE, 10.23),
+    (2, 'Gabumon', '15/11/2018', 2, TRUE, 8),
+    (3, 'Pikachu', '07/01/2021', 1, FALSE, 15.04),
+    (4, 'Devimon', '12/05/2017', 5, TRUE, 11);
