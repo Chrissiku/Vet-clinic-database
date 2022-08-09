c6f924349594b535f44eef731fdf63cb920d2ec8
chrissiku <chrissiku5@gmail.com>
2022-08-09 15:35:44 +0200

    Update all animals' weights that are negative to be their weight multiplied by -1

--- queries.sql
+++ queries.sql
@@ -97,5 +97,10 @@ SELECT * FROM animals;
 BEGIN;
 SAVEPOINT sp1;
 DELETE FROM animals WHERE date_of_birth > '01/01/2022';
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg  = (weight_kg * (-1));
 -- Rollback to the savepoint
 ROLLBACK TO sp1;
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg  = (weight_kg * (-1)) WHERE weight_kg < 0;
+
