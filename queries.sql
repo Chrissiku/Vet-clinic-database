04da3607cd9986e9b5ab95ccff699ac1e40c8dd3
chrissiku <chrissiku5@gmail.com>
2022-08-09 15:36:46 +0200

    Commir transaction

--- queries.sql
+++ queries.sql
@@ -103,4 +103,5 @@ UPDATE animals SET weight_kg  = (weight_kg * (-1));
 ROLLBACK TO sp1;
 -- Update all animals' weights that are negative to be their weight multiplied by -1.
 UPDATE animals SET weight_kg  = (weight_kg * (-1)) WHERE weight_kg < 0;
-
+-- Commit the transaction.
+COMMIT;
