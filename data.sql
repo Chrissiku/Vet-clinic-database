562442b5445689ecc582cf26905f3d2a3e825264
chrissiku <chrissiku5@gmail.com>
2022-08-10 13:45:02 +0200

    Update owner_id column of the animals table

--- data.sql
+++ data.sql
@@ -58,3 +58,10 @@ UPDATE
 SET
     species_id = 1
 WHERE species_id IS NULL;
+
+-- Edit owner_id column for the animals table.
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
