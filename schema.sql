02b6b5e4e3302d655b96c9b36e06fb6723521c1f
jheart-vic <adebowalevictorjheart.com>
2022-08-15 21:45:46 +0100

    Add index created to schema,sql

--- schema.sql
+++ schema.sql
@@ -97,3 +97,10 @@ ALTER COLUMN
 
 -- Create Index on animal_id column of visits table.
 CREATE INDEX animal_id_asc ON visits(animal_id asc);
+
+-- Create Index on vet_id column of visits table.
+CREATE INDEX  vet_id_asc ON visits(vet_id asc);
+
+-- Create Index  owners_email on email column of owners table.
+CREATE INDEX  owners_email  ON owners(email);
+
